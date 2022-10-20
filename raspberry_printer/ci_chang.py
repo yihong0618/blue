9f052abfe83dd1892e7eec2090823f99649dae7e
yihong0618 <zouzou0208@gmail.com>
2022-10-20 18:56:20 +0800

    fix: ci_chang text

--- raspberry_printer/ci_chang.py
+++ raspberry_printer/ci_chang.py
@@ -5,7 +5,7 @@ from raspberry_printer.config import MY_LEARNING_BOOK_RESOURCE
 
 
 def make_kai_xin_learning_text(data):
-    info_time = f"Time: {str(datetime.now())}"
+    info_time = f"Time: {str(datetime.now())}\n\n"
     unit_list = data.get("unit_list")
     if not unit_list:
         # first three units
@@ -13,4 +13,4 @@ def make_kai_xin_learning_text(data):
     text = "新的单元单词 str(unit_list)"
     dt = pd.read_csv(MY_LEARNING_BOOK_RESOURCE)
     dt = dt[dt["UnitID"].isin(unit_list)]
-    return "\n".join(dt["Word"].to_list())
+    return info_time + text + "\n".join(dt["Word"].to_list())
